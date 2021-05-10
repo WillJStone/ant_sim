246ce150857b8cb2fa4f950567a08ff3a6f3332f
asdf <asdf>
2021-05-10 20:30:28 -0300

    Changed ant model to colony

--- src/colony.rs
+++ src/colony.rs
@@ -1,7 +1,15 @@
+const NUM_ANTS: usize = 10;
+
+
 pub struct Ant {
     pub movement_speed: u8,
     pub perception_distance: u8,
     pub has_food: bool
+}
+
+
+pub struct Colony {
+    ants: [Ant; NUM_ANTS],
 }
 
 
@@ -11,6 +19,15 @@ impl Ant {
             movement_speed: 1,
             perception_distance: 3,
             has_food: false,
+        }
+    }
+}
+
+
+impl Colony {
+    pub fn new() -> Colony {
+        Colony {
+            ants: [Ant::new(); NUM_ANTS],
         }
     }
 }
