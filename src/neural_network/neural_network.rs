7ea9f5c2259b6cd298214644be0e541ed9f7c20c
will <will.stone@reeldata.ai>
2021-05-21 14:46:26 -0300

    Wrote MLP code and tests, tests passing

--- src/neural_network/neural_network.rs
+++ src/neural_network/neural_network.rs
@@ -23,9 +23,100 @@ impl Layer {
         }
     }
 
+    fn from_existing_weights(weights: Array<f32, Dim<[usize; 2]>>, bias: Array<f32, Dim<[usize; 2]>>) -> Layer {
+        Layer {
+            w: weights, 
+            b: bias,
+        }
+    }
+
     fn forward(&self, input: Array<f32, Dim<[usize; 2]>>) -> Array<f32, Dim<[usize; 2]>> {
         let h = input.dot(&self.w) + &self.b;
 
         relu(h)
+    }
+}
+
+
+struct MLP {
+    layers: Vec<Layer>,
+}
+
+
+impl MLP {
+    pub fn new(input_dimension: usize, hidden_sizes: Vec<usize>) -> MLP {
+        // NOTE: The last element of the hidden_sizes vector is the output size
+        let mut layers: Vec<Layer> = Vec::new();
+        let mut num_input = input_dimension;
+        for i in 0..hidden_sizes.len() {
+            let layer = Layer::new(num_input, hidden_sizes[i]);
+            layers.push(layer);
+            num_input = hidden_sizes[i];
+        }
+
+        MLP { layers }
+    }
+
+    pub fn forward(&self, input: Array<f32, Dim<[usize; 2]>>) -> Array<f32, Dim<[usize; 2]>> {
+        let mut layer_result = input;
+        for layer in self.layers.iter() {
+            layer_result = layer.forward(layer_result);
+        }
+
+        layer_result
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn new_layer() {
+        let layer = Layer::new(8, 4);
+        assert_eq!(layer.w.len(), 32);
+        assert_eq!(layer.b.len(), 4);
+    }
+
+    #[test]
+    fn test_from_existing_weights() {
+        let distribution = Uniform::new(-0.5, 0.5);
+        let w = Array::random((8, 4), distribution);
+        let b = Array::random((1, 4), distribution);
+
+        let layer = Layer::from_existing_weights(w.clone(), b.clone());
+
+        assert_eq!(layer.w, w);
+        assert_eq!(layer.b, b);
+
+    }
+
+    #[test]
+    fn test_layer_forward() {
+        let w: Array<f32, Dim<[usize; 2]>> = Array::eye(4);
+        let b: Array<f32, Dim<[usize; 2]>> = Array::zeros((1, 4));
+        let sample_input = Array::ones((1, 4));
+        let layer = Layer::from_existing_weights(w, b);
+
+        let out = layer.forward(sample_input.clone());
+
+        assert_eq!(sample_input, out);
+    }
+
+    #[test]
+    fn test_mlp_new() {
+        let mlp = MLP::new(4, vec![2, 2]);
+
+        assert_eq!(mlp.layers.len(), 2);
+        assert_eq!(mlp.layers[0].w.len(), 8);
+    }
+
+    #[test]
+    fn test_mlp_forward() {
+        let mlp = MLP::new(4, vec![2, 2]);
+        let sample_input = Array::ones((1, 4));
+        let sample_output = mlp.forward(sample_input);
+
+        assert_eq!(sample_output.len(), 2);
     }
 }
--- src/neural_network/utils.rs
+++ src/neural_network/utils.rs
@@ -1,5 +1,6 @@
 use ndarray::{Array, Dim};
 
+
 pub fn relu(array: Array<f32, Dim<[usize; 2]>>) -> Array<f32, Dim<[usize; 2]>> {
     fn _relu(x: f32) -> f32 {
         if x > 0.0 {
@@ -10,4 +11,17 @@ pub fn relu(array: Array<f32, Dim<[usize; 2]>>) -> Array<f32, Dim<[usize; 2]>> {
     }
 
     array.mapv_into(|x| _relu(x))
+}
+
+
+pub fn concat(arrays: &[Array<f32, Dim<[usize; 1]>>]) -> Array<f32, Dim<[usize; 1]>> {
+    let mut new_array: Vec<f32> = Vec::new();
+    
+    for array in arrays.iter() {
+        for elem in array.iter() {
+            new_array.push(*elem);
+        }
+    }
+    
+    Array::from(new_array)
 }
